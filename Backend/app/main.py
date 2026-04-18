0aea2423211857353171112e2515cbe006ecc0f4
Juanca <jcpabon03@gmail.com>
2026-04-18 08:57:52 -0500

    Add BaseHTTPMiddleware import to main.py

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from starlette.middleware.base import BaseHTTPMiddleware
 
 from app.database import Base, engine
 from app.routers import auth_router
