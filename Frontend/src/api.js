9d2b3ce4ac8d9ef00c5c248477f17c985883a6fe
Juanca <jcpabon03@gmail.com>
2026-04-17 20:01:49 -0500

    Change axios baseURL to use environment variable

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000'  // ← esto faltaba
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 instance.interceptors.request.use((config) => {
