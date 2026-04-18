fb4f89a1f92d6566a64e72f18bacc77565d80c07
Juanca <jcpabon03@gmail.com>
2026-04-17 21:10:19 -0500

    Fix formatting issues in main.py

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -11,6 +11,8 @@ from app.routers import public_menu_router
 
 app = FastAPI(
     title="JucaMenu API"
+    redirect_slashes=False
+    
 )
 
 # ── 1. CORS (CONFIGURACIÓN DEFINITIVA) ─────────────────────
